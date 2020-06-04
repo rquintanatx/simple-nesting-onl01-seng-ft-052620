a1e7355b139ef4352ee30905251776e0c924165b
Robert Quintana <robertquintanatx@gmail.com>
2020-06-04 11:04:15 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -61,7 +61,7 @@ def dennis_ritchies_language
         }
      }
   
-  programmer_hash[:dennis_ritchie][:languages].split
+  programmer_hash[:dennis_ritchie][:languages].to_a
 end
 
 def adding_matz
